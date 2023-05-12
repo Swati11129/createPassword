c073fd4761745448329de7243583221f8662f780
Swati <swati11129@gmail.com>
2023-05-12 20:55:51 +0530

    added logo

--- src/components/CreatePassword.vue
+++ src/components/CreatePassword.vue
@@ -1,12 +1,18 @@
 <template>
+  <div class="d-flex" style="height:100vh;">
+
+    <div>
+      <img src="../assets/cloudnuro logo.png" style="position:absolute; top:20px; left:20px; width:80px"/>
+    </div>
+  
   <v-container class="" style="margin: auto; width: 540px">
     <div class=" text-left">
       <div class="font-weight-bold text-h5">Hello Roger George</div>
       <div class="mb-6">Create new password</div>
 
       <div class="mb-6 font-weight-bold d-flex" style="gap: 10px;">
-        <div style="gap:3px">
-            <v-icon color=""  > mdi-email </v-icon>
+        <div >
+            <v-icon> mdi-email </v-icon>
         </div>
         <div >
             roger.george@cloudnuro.com
@@ -27,13 +33,14 @@
         <input type="password" v-model="confirmPassword" @focus="removeConfirmWarning" @blur="showConfirmWarningMethod" class="mb-3" />
       </div>
       <div v-if="showWarning"  class="red--text mb-3" >{{ warningText }}</div>
-      <div v-else-if="showConfirm"  class="red--text mb-3" >{{ confirmWarningText }}</div>
+      <div v-else-if="showConfirm" class="red--text mb-3" >{{ confirmWarningText }}</div>
       
-      <div class="  text-right ">
-        <v-btn  :disabled="showConfirm || showWarning" class="" style=" ">Create</v-btn>
+      <div class="text-right ">
+        <v-btn  :disabled="showConfirm || showWarning" >Create</v-btn>
       </div>
     </div>
   </v-container>
+</div>
 </template>
 <script>
 export default {
