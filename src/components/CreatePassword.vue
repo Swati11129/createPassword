b1b1aacc2acad7bf4e45eb298cc8d3f380c39025
Swati <swati11129@gmail.com>
2023-05-19 10:03:59 +0530

    used rule props

--- src/components/CreatePassword.vue
+++ src/components/CreatePassword.vue
@@ -1,27 +1,26 @@
 <template>
-  <div class="d-flex" style="height: 100vh;">
-    
+  <div class="d-flex" style="height: 100vh">
     <img
       src="../assets/cloudnuro logo.png"
       style="position: absolute; top: 20px; left: 20px; width: 80px"
     />
 
-    <v-container style="margin: auto; width: 540px" class=" text-left">
-        <div class="font-weight-bold text-h5">Hello Roger George</div>
-        <div class="mb-6">Create new password</div>
-        <div class="mb-6 font-weight-bold d-flex" style="gap: 10px">
-          <div>
-            <v-icon> mdi-email </v-icon>
-          </div>
-          <div>roger.george@cloudnuro.com</div>
-        </div>
-        <div class="mb-3">
-          Create a strong password! Use a combination of alphabets, numbers,
-          special symbols and minimum of 8 characters
-        </div>
+    <v-container style="margin: auto; width: 540px" class="text-left">
+      <v-form ref="form" v-model="valid" >
+      <div class="font-weight-bold text-h5">Hello Roger George</div>
+      <div class="mb-6">Create new password</div>
+      <div class="mb-6 font-weight-bold d-flex" style="gap: 10px">
         <div>
-          <label>Create Password</label>
-          <br />
+          <v-icon> mdi-email </v-icon>
+        </div>
+        <div>roger.george@cloudnuro.com</div>
+      </div>
+      <div class="mb-3">
+        Create a strong password! Use a combination of alphabets, numbers,
+        special symbols and minimum of 8 characters
+      </div>
+      <div>
+        <div>Create Password</div>
           <v-text-field
             dense="false"
             :type="show1 ? 'text' : 'password'"
@@ -29,16 +28,14 @@
             :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
             outlined
             v-model="password"
-            @focus="removeWarning"
-            @blur="showCreateWarningMethod"
+            :rules="[rules.createPasswordRequired, rules.strongPassword]"
             color="red"
-            style="height: 50px"
+            @blur="validate"
+            @focus="validate"
           />
-        </div>
-        <div v-if="showCreateWarning" class="red--text">{{ createWarningText }}</div>
-        <div>
-          <label>Confirm Password</label>
-          <br />
+      </div>
+      <div>
+        <div>Confirm Password</div>
           <v-text-field
             dense="false"
             :type="show2 ? 'text' : 'password'"
@@ -46,19 +43,14 @@
             :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
             outlined
             v-model="confirmPassword"
-            @focus="removeConfirmWarning"
-            @blur="showConfirmWarningMethod"
+            :rules="[rules.confirmPasswordRequired, rules.matchPassword]"
             color="red"
-            style="height: 50px"
           />
         </div>
-        <div v-if="showConfirmWarning" class="red--text">
-          {{ confirmWarningText }}
-        </div>
-
         <div class="text-right">
-          <v-btn :disabled="showConfirmWarning || showCreateWarning || password!==confirmPassword " >Create</v-btn>
+          <v-btn @click="validate" :disabled="!valid">Create</v-btn>
         </div>
+      </v-form>
     </v-container>
   </div>
 </template>
@@ -68,63 +60,27 @@ export default {
   data() {
     return {
       show1: false,
+      valid:true,
       show2: false,
       password: "",
       confirmPassword: "",
-      createWarningText: "",
-      confirmWarningText: "",
-      showCreateWarning: false,
-      showConfirmWarning: false,
+      rules: {
+        createPasswordRequired: (v) =>v.length != 0 || "Password Required",
+        minLength: (v) =>v.length >= 8 || "New Password must be at least 8 characters",
+        strongPassword: (v) =>
+         ( /[a-z]+/.test(v) && /[A-Z]+/.test(v) && /[0-9]+/.test(v) && /[@!]+/.test(v) )||
+          "Password not strong. Add: ! @, 123, abc, ABC",
+        confirmPasswordRequired: (v) =>v.length != 0 || "Password must be there",
+        matchPassword: (v) => v=== this.password || "Password not matching"
+      },
     };
   },
   methods: {
-   
-    
-    removeWarning() {
-      this.showCreateWarning = false;
-    },
-    removeConfirmWarning() {
-      this.showConfirmWarning = false;
-    },
-    showCreateWarningMethod() {
-      if (this.password.length === 0) {
-        this.showCreateWarning=true;
-        this.createWarningText = "Password Required";
-      } else if (this.password.length < 8) {
-        this.showCreateWarning = true;
-        this.createWarningText = "New Password must be at least 8 characters";
-      } else if (!/[a-z]/.test(this.password)) {
-        this.showCreateWarning = true;
-        this.createWarningText = "Password not strong. Add: ! @, 123, abc, ABC";
-      } else if (!/[A-Z]/.test(this.password)) {
-        this.showCreateWarning = true;
-        this.createWarningText = "Password not strong. Add: ! @, 123, abc, ABC";
-      } else if (!/[0-9]/.test(this.password)) {
-        this.showCreateWarning = true;
-        this.createWarningText = "Password not strong. Add: ! @, 123, abc, ABC";
-      } else if (!/[@!]/.test(this.password)) {
-        this.showCreateWarning = true;
-        this.createWarningText = "Password not strong. Add: ! @, 123, abc, ABC";
-      } else {
-        this.showCreateWarning = false;
-      }
-    },
-    showConfirmWarningMethod() {
-      if (this.showCreateWarning == false) {
-        if (this.confirmPassword == "") {
-          this.showConfirmWarning = true;
-          this.confirmWarningText = "Password must be there";
-        } else if (this.password != this.confirmPassword) {
-          this.showConfirmWarning = true;
-          this.confirmWarningText = "Password not matching";
-        } else {
-          this.showConfirmWarning = false;
-        }
-      } 
-      else {
-        this.showConfirmWarning = false;
-      }
-    },
+    validate () {
+         this.$refs.form.validate()
+      },
+
   },
+
 };
 </script>
